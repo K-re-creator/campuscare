96f643a38b0019b7653da863168f569c16c3d099
K-re-creator <koziresy11@gmail.com>
2026-09-20 12:40:42 +0300

    Initialize CampusCare laboratory project

---
+++ public/assets/index.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+$requestId = bin2hex(random_bytes(4));
+?>
+<!doctype html>
+<html lang="en">
+<head><meta charset="utf-8"><title>CampusCare</title></head>
+<body>
+
+<main>
+<h1>CampusCare Service Booking</h1>
+<p>Development request: <?= htmlspecialchars($requestId) ?></p>
+</main>
+</body>
+</html>
